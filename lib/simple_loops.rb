6003ad48a3d059ab9c47d28c90f45ead3eb68951
Jingchao Yu <jingchaoyu@hotmail.com>
2021-01-01 18:27:54 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
-# Write your methods here
+def loop_message_five_times(msg)
+  5.times do
+    puts msg
+  end
+end
+
+def loop_message_n_times(msg, time)
+  time.times do
+    puts msg
+  end
+end
+  
+
+
+describe 'output_array' do
+
+  it 'takes in a message and a number outputs the message that number of times' do
+    array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+    expect { output_array(array) }.to output(/Be yourself;\neveryone else is already taken\n- Oscar Wilde/).to_stdout, "Expected:\n\nBe yourself;\neveryone else is already taken\n- Oscar Wilde"
+    array = [5, 4, 3, 2, 1]
+    expect { output_array(array) }.to output(/5\n4\n3\n2\n1\n/).to_stdout
+  end
+end
+
+describe 'return_string_array' do
+  it 'takes in an array of elements, converts them to strings and returns them all in a new array' do
+    array = [5, 4, 3, 2, 1]
+    expect(return_string_array(array)).to eq(["5", "4", "3", "2", "1"])
+  end
+end
--- spec/simple_loops_spec.rb
+++ spec/simple_loops_spec.rb
@@ -21,19 +21,22 @@ describe 'loop_message_n_times' do
 
 end
 
-describe 'output_array' do
-
-  it 'takes in a message and a number outputs the message that number of times' do
-    array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
-    expect { output_array(array) }.to output(/Be yourself;\neveryone else is already taken\n- Oscar Wilde/).to_stdout, "Expected:\n\nBe yourself;\neveryone else is already taken\n- Oscar Wilde"
-    array = [5, 4, 3, 2, 1]
-    expect { output_array(array) }.to output(/5\n4\n3\n2\n1\n/).to_stdout
+def output_array(arr)
+  x = 0
+  while arr[x] do
+    puts arr[x]
+    x += 1
   end
 end
 
-describe 'return_string_array' do
-  it 'takes in an array of elements, converts them to strings and returns them all in a new array' do
-    array = [5, 4, 3, 2, 1]
-    expect(return_string_array(array)).to eq(["5", "4", "3", "2", "1"])
+def return_string_array(arr)
+  x = 0 
+  new_arr = []
+  while arr[x] do
+    new_arr[x] = arr[x].to_s
+    x += 1
   end
+  return new_arr
 end
+
+
